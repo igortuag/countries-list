2795b180031ea488c0a9466464586382d28b794d
Igor Tuag <igortuag@gmail.com>
2025-05-19 09:30:13 -0300

    feat: add CountryCard component and implement country routing; remove unused Welcome and Test components

---
+++ app/components/CountryCard.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router";
+
+export const CountryCard = () => {
+  return (
+    <Link to="/country/germany" className="p-3 bg-white rounded-2xl">
+      <img
+        className="block w-full object-contain"
+        src="https://flagcdn.com/w320/de.png"
+      />
+      <h3 className="text-2xl font-bold py-4 text-center">Alemanha</h3>
+    </Link>
+  );
+};
