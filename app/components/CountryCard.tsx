c8f1f2ffc86aa594aa4f9872e102b63dcc59f231
Igor Tuag <igortuag@gmail.com>
2025-05-19 18:00:57 -0300

    feat: implement country list page with CountryCard component and update routing

--- app/components/CountryCard.tsx
+++ app/components/CountryCard.tsx
@@ -4,7 +4,7 @@ export const CountryCard = () => {
   return (
     <Link to="/country/germany" className="p-3 bg-white rounded-2xl">
       <img
-        className="block w-full object-contain"
+        className="block w-full object-contain rounded-2xl"
         src="https://flagcdn.com/w320/de.png"
       />
       <h3 className="text-2xl font-bold py-4 text-center">Alemanha</h3>
